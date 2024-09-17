06c408362429c82fbd9ef3f7095d940dda62a34f
yunqiji <xiaoxiongchenCXXcc@outlook.com>
2024-09-17 10:18:47 +0800

    UploadThing setup
    增加上传图片组件；完成整个表单提交的测试

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import {db} from "@/lib/db"
 import { redirect } from "next/navigation";
-import InitialModel from "@/components/models/initialModel";
+import InitialModel from "@/components/models/initial-modal";
 
 const SetupPage = async () => {
 
--- components/models/initial-modal.tsx
+++ components/models/initial-modal.tsx
@@ -24,6 +24,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import FileUpload from '@/components/FileUpload';
 
 
 const formSchema = z.object({
@@ -80,7 +81,21 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Upload Image
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({field})=>(
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
               <FormField
