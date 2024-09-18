4fe53e96e3997bf132fff36daa3c11c48e66e6fa
yunqiji <xiaoxiongchenCXXcc@outlook.com>
2024-09-18 11:15:38 +0800

    Server Sidebar
    1. 完成服务的页面布局，增加 serverPage 侧边栏
    2. 侧边栏增加下拉框选项

---
+++ components/server/server-header.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { ServerWithMembersWithProfiles } from "@/types";
+import { MemberRole } from "@prisma/client";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus, 
+  Users
+} from "lucide-react";
+
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfiles;
+  role?: MemberRole;
+}
+
+const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerrator = isAdmin || role === MemberRole.MODERATOR;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none" asChild>
+        <button
+          className="w-full text-md font-semibold px-3 flex 
+        items-center h-12 border-neutral-200
+         dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10
+          dark:hover:bg-zinc-700/50 transition"
+        >
+          {server.name}
+          <ChevronDown className="h-5 w-5 ml-auto" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-56 text-xs font-medium text-black
+        dark:text-neutral-400 space-y-[2px]"
+      >
+        {isModerrator && (
+          <DropdownMenuItem
+            className="text-indigo-600 dark:text-indigo-400
+                px-3 py-2 text-sm cursor-pointer"
+          >
+            Invite Peple
+            <UserPlus className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerrator && (
+          <DropdownMenuItem
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Server Setting
+            <Settings className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerrator && (
+          <DropdownMenuItem
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Manage Memeber
+            <Users className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerrator && (
+          <DropdownMenuItem
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Create Channel
+            <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerrator && (
+          <DropdownMenuSeparator/>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Lever Server
+            <LogOut className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ServerHeader;
